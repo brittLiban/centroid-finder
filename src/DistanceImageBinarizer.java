10a41879dedca9244da69d6910587902d2f86bb3
brittLiban <britt.liban@student.greenriver.edu>
2025-04-21 11:59:05 -0700

    Comments on hexacode and how it works in distanceImageBinarizer. Going to be working on extracting each r g and b then comparing those individual digits to the ones in the for loop below and checking it its within threshold, if yes, then 1, if no then 0;

--- src/DistanceImageBinarizer.java
+++ src/DistanceImageBinarizer.java
@@ -4,14 +4,18 @@ import java.awt.image.BufferedImage;
  * An implementation of the ImageBinarizer interface that uses color distance
  * to determine whether each pixel should be black or white in the binary image.
  * 
- * The binarization is based on the Euclidean distance between a pixel's color and a reference target color.
- * If the distance is less than the threshold, the pixel is considered white (1);
+ * The binarization is based on the Euclidean distance between a pixel's color
+ * and a reference target color.
+ * If the distance is less than the threshold, the pixel is considered white
+ * (1);
  * otherwise, it is considered black (0).
  * 
- * The color distance is computed using a provided ColorDistanceFinder, which defines how to compare two colors numerically.
+ * The color distance is computed using a provided ColorDistanceFinder, which
+ * defines how to compare two colors numerically.
  * The targetColor is represented as a 24-bit RGB integer in the form 0xRRGGBB.
  * 
- * // Liban - Some initial thoughts - consider translating the rgb code to the actual number?
+ * // Liban - Some initial thoughts - consider translating the rgb code to the
+ * actual number?
  */
 public class DistanceImageBinarizer implements ImageBinarizer {
     private final ColorDistanceFinder distanceFinder;
@@ -22,14 +26,18 @@ public class DistanceImageBinarizer implements ImageBinarizer {
      * Constructs a DistanceImageBinarizer using the given ColorDistanceFinder,
      * target color, and threshold.
      * 
-     * The distanceFinder is used to compute the Euclidean distance between a pixel's color and the target color.
+     * The distanceFinder is used to compute the Euclidean distance between a
+     * pixel's color and the target color.
      * The targetColor is represented as a 24-bit hex RGB integer (0xRRGGBB).
-     * The threshold determines the cutoff for binarization: pixels with distances less than
+     * The threshold determines the cutoff for binarization: pixels with distances
+     * less than
      * the threshold are marked white, and others are marked black.
      *
      * @param distanceFinder an object that computes the distance between two colors
-     * @param targetColor the reference color as a 24-bit hex RGB integer (0xRRGGBB)
-     * @param threshold the distance threshold used to decide whether a pixel is white or black
+     * @param targetColor    the reference color as a 24-bit hex RGB integer
+     *                       (0xRRGGBB)
+     * @param threshold      the distance threshold used to decide whether a pixel
+     *                       is white or black
      */
     public DistanceImageBinarizer(ColorDistanceFinder distanceFinder, int targetColor, int threshold) {
         this.distanceFinder = distanceFinder;
@@ -54,6 +62,21 @@ public class DistanceImageBinarizer implements ImageBinarizer {
         //getting the width and height of the image
         int width  = image.getWidth();
         int height = image.getHeight();
+
+        //evaluating it for the red
+        // this whats in the front over 16 bits/or really shifts everything down. 
+        //255 to see what our red is 
+
+        //remember
+        // FF = 11111111
+        // if a number is not 1 it will return 0
+        // - essentially remember with hexacode, if a number is for ex in the 16th place,
+        //mult that number by 16 and you get that number.
+        // when in hexacode each individual digit represetns 4 in binary
+        // so when evaluating a hexacode # - do 16^x - x being what ever place it is in. 
+        // Example: 0x7C = (7 * 16^1) + (C * 16^0)
+        //        = (7 * 16) + (12 * 1) = 112 + 12 = 124
+        int r = (targetColor >> 16) & 0xFF;
 
 
         /*
@@ -95,15 +118,16 @@ public class DistanceImageBinarizer implements ImageBinarizer {
      * Black pixels are encoded as 0x000000 and white pixels as 0xFFFFFF.
      *
      * @param image a 2D array of 0s and 1s representing the binary image
-     * @return a BufferedImage where black and white pixels are represented with standard RGB hex values
+     * @return a BufferedImage where black and white pixels are represented with
+     *         standard RGB hex values
      */
     @Override
     public BufferedImage toBufferedImage(int[][] image) {
-        //image is getting passed in as a 2d array
-        //number of rows and columns in binary array
+        // image is getting passed in as a 2d array
+        // number of rows and columns in binary array
         int height = image.length;
-        int width  = image[0].length;
-        
+        int width = image[0].length;
+
         // create an empty RGB image
         BufferedImage out = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
