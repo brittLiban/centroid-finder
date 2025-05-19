da0171bc61580e802e3d7a002952544d62004488
brittLiban <britt.liban@student.greenriver.edu>
2025-05-19 15:31:50 -0700

    Borrowing some from the ImageSummaryApp

--- src/main/java/io/github/brittLiban/centroidfinder/VideoProcessor.java
+++ src/main/java/io/github/brittLiban/centroidfinder/VideoProcessor.java
@@ -1,11 +1,29 @@
 package io.github.brittLiban.centroidfinder;
 
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
 public class VideoProcessor {
     public static void main(String[] args) {
         if (args.length < 3) {
-            System.out.println("Usage: java ImageSummaryApp <input_image> <hex_target_color> <threshold>");
+            System.out.println("Usage: java -jar videoprocessor.jar <inputPath> <outputCsv> <targetColor> <threshold>");
+        return;
+        }
+
+        String inputPath = args[0];
+        String outPutCSV = args[1];
+        String targetColorHex = args[2];
+        int threshold = 0;
+
+        try {
+            threshold = Integer.parseInt(args[2]);
+        } catch (NumberFormatException e) {
+            System.err.println("Threshold must be an integer.");
             return;
         }
+
+        
 
     }
 }
