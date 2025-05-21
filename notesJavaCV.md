48287c94c909c1bc52e3d7fcebee2ba25f6a5193
brittLiban <britt.liban@student.greenriver.edu>
2025-05-20 22:06:10 -0700

    it runs!

---
+++ notesJavaCV.md
@@ -0,0 +1,6 @@
+How to use JavaCV
+
+- Good resource for reading things in and videos
+
+https://reintech.io/blog/java-video-processing-streaming-manipulating-video-files
+
--- src/main/java/io/github/brittLiban/centroidFinder/VideoProcessorApp.java
+++ src/main/java/io/github/brittLiban/centroidFinder/VideoProcessorApp.java
@@ -1,5 +1,50 @@
 package io.github.brittLiban.centroidFinder;
 
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+
 public class VideoProcessorApp {
-    
+    public static void main(String[] args){
+        //taking in the command args
+        if (args.length < 4) {
+            System.out.println("Usage: java VideoProcessorApp inputPath outputCsv targetColor threshold");
+            return;
+        }
+
+        //reading in the args
+        String inputVideoPath = args[0];
+        String hexTargetColor = args[1];
+        String outPutCsv = args[2];
+        int threshold = 0;
+
+
+        //validating the args
+
+        try(FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputVideoPath)){
+            grabber.start();
+            System.out.println("Total frames: " + grabber.getLengthInFrames());
+            System.out.println("Video duration (s): " + grabber.getLengthInTime() / 1_000_000.0);
+            grabber.stop();
+        } catch(Exception e) {
+            System.out.println("Failed to run the video");
+        }
+
+        try {
+            threshold = Integer.parseInt(args[3]);
+        } catch (NumberFormatException e) {
+            System.err.println("Threshold must be an integer.");
+            return;
+        }
+
+        // Parse the target color from a hex string (format RRGGBB) into a 24-bit integer (0xRRGGBB)
+        int targetColor = 0;
+        try {
+            targetColor = Integer.parseInt(hexTargetColor, 16);
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid hex target color. Please provide a color in RRGGBB format.");
+            return;
+        }
+
+
+
+    }//end of main 
 }
