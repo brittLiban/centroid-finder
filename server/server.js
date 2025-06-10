ab8ed51c0ff011611c2a4e1bb1776ed77d23e059
LibanBritt <liban3367@gmail.com>
2025-06-10 10:50:52 -0700

    Work in progress. Need to fix the java stderr

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import router from './router/routes.js';
 import path from 'path';
+import cors from 'cors';
 
 const app = express();
 //be able to handle form data (spec for adding products)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
+
 
 //telling it to serve public files
 app.use(express.static('./public'));
@@ -18,8 +21,8 @@ app.use('/results', express.static(path.resolve('outputCsv')));
 
 app.use("/", router);
 
-app.listen(3000, () => {
-    console.log(`Server is running on http://localhost:3000`);
+app.listen(3001, () => {
+    console.log(`Server is running on http://localhost:3001`);
 });
 
 //exporting for testing purposes
